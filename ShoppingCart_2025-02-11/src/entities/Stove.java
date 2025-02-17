7e4ac93fc4b4255627c4f852d68a3e248d1507a5
Gabriel Calixto <gntdc@ic.ufal.br>
2025-02-17 02:45:34 -0300

    02-17-2025 Questão 2

---
+++ ShoppingCart_2025-02-11/src/entities/Stove.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Stove extends Product {
+    private int burners;
+
+    public Stove(String name, double price, int burners) {
+        super(name, price);
+        this.burners = burners;
+    }
+    public int getBurners() {
+        return burners;
+    }
+}
